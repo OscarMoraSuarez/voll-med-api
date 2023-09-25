43db426855e92d14e2feaf1e0be7049b2b7efd68
RaJSo <yinei505@gmail.com>
2023-09-25 01:05:05 -0500

    implementacion de dependencia security creacion de repo entidad y migracion

---
+++ src/main/java/med/voll/api/Dominio/usuarios/UsuarioRepositorio.java
@@ -0,0 +1,10 @@
+package med.voll.api.Dominio.usuarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepositorio extends JpaRepository<Usuario,Long> {
+
+
+    UserDetails findByUsername(String username);
+}
